eb921c3c6a11731a4d3b1258d23bfb417b79c5aa
Timon Lukas <timon.lukas3@gmail.com>
2021-05-27 16:19:07 +0200

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:TimonLukas/ant-incremental.git main:gh-pages
+git push -f git@github.com:TimonLukas/ant-incremental.git master:gh-pages
 
 cd -
